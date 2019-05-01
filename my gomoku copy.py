8d72a0bf96880d2128e014e553001f1656e9cdf4
Renlii <49928737+Renlii@users.noreply.github.com>
2019-05-01 11:36:35 -0400

    Add files via upload

---
+++ my gomoku copy.py
@@ -0,0 +1,196 @@
+#import packages
+import pygame 
+from pygame.locals import *
+import sys
+
+    
+#about screen
+Screen= width, height = 1440,960
+
+
+#init the game
+pygame.init()
+
+#setup screen
+Screen = pygame.display.set_mode(Screen)
+pygame.display.set_caption("Gomoku")
+
+#define colors
+BLACK = ( 0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = ( 0, 255, 0)
+BLUE = ( 0, 0, 255)
+
+
+font = pygame.font.Font(None, 40)
+text_black = font.render('Black win!', True, GREEN, BLUE)
+text_white = font.render('White win!', True, GREEN, BLUE)
+textRect_black = text_black.get_rect()
+textRect_white = text_white.get_rect()
+textRect_black.center = (1440 // 2, 960 // 2)
+textRect_white.center = (1440 // 2, 960 // 2)
+
+game_over = False
+n=1 #n 用于判断谁改下了 当n为单数时 后手下子  n为偶数时 先手下子
+all_black=[]
+all_white=[]
+time_black_win=0
+time_white_win=0
+
+def who_wins(color_of_list, new_position):
+    """color_of_list 代表了谁在下子（那个颜色） new_position 代表了新下的那个子的位置 我们需要对比新\
+    下的子的位置和所有原来在list中的位置 看看有没有五个连续的 如果有五个连续的则判定胜利"""
+    global black_1
+    global white_1
+    global display_1
+    a=0
+    black_1=False
+    white_1=False
+    display_1=a
+    
+    for i in range(1,5):
+        # horizontal
+        if [int(new_position[0])+25*i,int(new_position[1])] in color_of_list:
+            a+=1
+        elif [int(new_position[0]) -25*i,int(new_position[1])] in color_of_list:
+            a+=1
+        # vertical
+        elif [int(new_position[0]), int(new_position[1])+25*i] in color_of_list:
+            a+=1
+        elif [int(new_position[0]), int(new_position[1])-25*i] in color_of_list:
+            a+=1
+        # up left, down right
+        elif [int(new_position[0])+25*i, int(new_position[1])-25*i] in color_of_list:
+            a+=1
+        elif [int(new_position[0])-25*i, int(new_position[1])+25*i] in color_of_list:
+            a+=1
+        # up right, down left
+        elif [int(new_position[0])+25*i, int(new_position[1])+25*i] in color_of_list:
+            a+=1
+        elif [int(new_position[0])-25*i, int(new_position[1])-25*i] in color_of_list:
+            a+=1
+        else:
+            print("value i:",i)
+            break
+    if a >= 4:
+        if (n-1)%2 == 1:
+            black_1=True
+            return(a)
+        if (n-1)%2 == 0:
+            white_1=True
+            return(a)
+    print(a)
+
+def RestartButton():
+    pygame.draw.line(Screen, BLACK, [1300,100], [1300,150], 2)
+    pygame.draw.line(Screen, BLACK, [1350,100], [1350,150], 2)
+    pygame.draw.line(Screen, BLACK, [1300,100], [1350,100], 2)
+    pygame.draw.line(Screen, BLACK, [1300,150], [1350,150], 2)
+    font = pygame.font.SysFont(None, 21)
+    text = font.render("Restart", True, RED)
+    Screen.blit(text, (1300, 160))
+    pygame.display.update()
+
+def ScreenSetUp():
+    global Screen
+    global boardImage
+    global restart_button
+    Screen.fill(WHITE)
+    boardImage = pygame.image.load('boardphoto.jpg')
+    Screen.blit(boardImage, (0,0))
+    #pygame.draw.line(Screen, Color, Start_position, End_position, Width)
+    #Draw the board
+    for i in range (50):
+        pygame.draw.line(Screen, BLACK, [25*i, 0], [25*i,950], 1)
+        pygame.draw.line(Screen, BLACK, [0,25*i], [1225, 25*i], 1)
+    RestartButton()
+    pygame.display.update()
+
+#所有的空的交点
+def Empty_intersection():
+    empty_int=[]
+    for i in range(50):
+        for a in range(39):
+            empty_int.append([25*i,25*a])
+    return empty_int
+empty_intersection = Empty_intersection()
+
+def reset():
+    global n
+    global all_black
+    global all_white
+    global time_black_win
+    global time_white_win
+    global game_over
+    global empty_intersection
+    pygame.init()
+    empty_intersection = Empty_intersection()
+    n=1 #n 用于判断谁改下了 当n为单数时 后手下子  n为偶数时 先手下子
+    all_black=[]
+    all_white=[]
+    time_black_win=0
+    time_white_win=0
+    game_over = False
+    ScreenSetUp()
+
+#print(empty_intersection)
+
+ScreenSetUp()
+while True:
+    for new_event in pygame.event.get():
+        if new_event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if new_event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            #print(pos)
+            pos_position = [round(x / 25) for x in pos]
+            #print(pos_position)
+            intersection_position = [pos_position[i] * 25 for i in range(len(pos_position))]
+            #print(intersection_position)
+            button_set = [[1300,100],[1300,125],[1300,150],[1325,100],[1325,125],[1325,150],[1350,100],[1350,125],[1350,150]]
+            if intersection_position in button_set:
+                reset()
+            if intersection_position in empty_intersection:
+                if n%2 == 1:
+                    n+=1
+                    pygame.draw.circle(Screen, BLACK, intersection_position, 10, 0)
+                    all_black.append(intersection_position)
+                    empty_intersection.remove(intersection_position)
+                    who_wins(all_black,intersection_position)
+                    #print('black_1 is',black_1)
+                    #print("n is",n)
+                    #print("a is",display_1)
+                    if black_1 == True:
+                        Screen.blit(text_black, textRect_black)
+                        pygame.display.update()
+                        time_black_win+=1
+                        font = pygame.font.SysFont(None, 80)
+                        reminder = font.render("GAME OVER!", True, BLACK)
+                        Screen.blit(reminder, reminder.get_rect())
+                        pygame.display.update()
+                        game_over = True
+                elif n%2 == 0:
+                    n+=1
+                    pygame.draw.circle(Screen, WHITE, intersection_position,10, 0)
+                    all_white.append(intersection_position)
+                    empty_intersection.remove(intersection_position)
+                    who_wins(all_white,intersection_position)
+                    if white_1 == True:
+                        Screen.blit(text_white, textRect_white)
+                        pygame.display.update()
+                        time_white_win+=1
+                        font = pygame.font.SysFont(None, 80)
+                        reminder = font.render("GAME OVER!", True, BLACK)
+                        Screen.blit(reminder, reminder.get_rect())
+                        pygame.display.update()
+                        game_over = True
+                pygame.display.update()
+    #get mouse position          
+    x, y = pygame.mouse.get_pos()
+    #print(x,y)
+
+pygame.quit()
+
+
